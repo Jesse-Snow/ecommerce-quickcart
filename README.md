021432419aed5b2d93fea5dacecf08570357701b
jesse-snow <jesse.neves@outlook.com>
2025-09-19 13:45:23 -0300

    Added mongoose to use mongodb Created db.js to handle db Connection Created User model

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Ecommerce App
+# Ecommerce App
+
+## Authentication
+https://dashboard.clerk.com
+
+## Database
+https://cloud.mongodb.com/v2/
---
+++ model/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  _id:{ type:String, required:true},
+  name: { type:String, required:true},
+  email: { type:String, required:true,unique:true},
+  imageurl: { type:String, required:true},
+  cartItems: {type:Object, default: {}}, // Items stored in user table?
+}, { minimize:false })
+
+const User = mongoose.models.user || mongoose.model('user', userSchema) // if model is available use, ifnot create user model
+
+export default User
