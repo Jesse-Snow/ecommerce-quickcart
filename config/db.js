bda8d44720eebd7d65c908943f07009e32b510a1
jesse-snow <jesse.neves@outlook.com>
2025-09-19 15:44:17 -0300

    Fixes

--- config/db.js
+++ config/db.js
@@ -19,7 +19,7 @@ async function connectDB(){
       bufferCommands: false
     }
 
-    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}`,opts).then(mongoose => {
+    cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart-database`,opts).then(mongoose => {
       return mongoose
     })
   }
--- config/ingest.js
+++ config/ingest.js
@@ -3,7 +3,7 @@ import connectDB from "./db"
 import User from "@/model/User";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "ecommerce=quickcart" });
+export const inngest = new Inngest({ id: "ecommerce-quickcart" });
 
 // Inngest Function to save user data to the database
 export const syncUserCreation = inngest.createFunction(
